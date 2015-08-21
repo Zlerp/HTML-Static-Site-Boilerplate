97578a9e51a1c5fa93ba1d6969a5b5aa2b3a042d
Zlerp <zhpdeals@gmail.com>
2015-08-21 01:19:09 -0400

    Add Features to Boiler

--- static_site_boiler.rb
+++ static_site_boiler.rb
@@ -1,6 +1,35 @@
 
-def open(shit)
-  puts shit
+require 'fileutils'
+
+def open(item)
+  case item
+  when "a"
+    puts "Font Awesome Added"
+    gemfile = File.open("Gemfile", "a+"){|f| f << "\ngem 'font-awesome-rails'"}
+  when "b"
+    puts "Burbon Added"
+    gemfile = File.open("Gemfile", "a+"){|f| f << "\ngem 'bourbon'"}
+  when "c"
+    puts "TwitterBootstrap Added"
+    html = File.open("index.html", "a+"){|f| f << "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css\">"}
+  when "d"
+    puts "Materialize Added"
+    html = File.open("index.html", "a+"){|f|
+      f << "
+        <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css\">
+        <script src=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js\"></script>
+      "}
+  when "d"
+    puts "Something Added"
+  when "e"
+    puts "Google's Material Design Added"
+    html = File.open("index.html", "a+"){|f|
+      f << "
+      <link rel=\"stylesheet\" href=\"https://storage.googleapis.com/code.getmdl.io/1.0.4/material.indigo-pink.min.css\">
+      <script src=\"https://storage.googleapis.com/code.getmdl.io/1.0.4/material.min.js\"></script>
+      <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/icon?family=Material+Icons\">
+      "}
+  end
 end
 
 
@@ -18,21 +47,28 @@ Dir.chdir(directory)
 puts Dir.pwd
 
 
-server = File.open("server.rb", "w") { |file| file.write("require 'sinatra'\n\nget '/' do\n\tFile.read(File.join('public/*', 'index.html'))\nend") }
+server = File.open("server.rb", "w") { |file| file.write("require 'sinatra'\n\nget '/' do\n\tFile.read(File.join('public/', 'index.html'))\nend") }
 procfile = File.open("Procfile", "w") { |file| file.write("web: ruby server.rb -p $PORT") }
 gemfile = File.open("Gemfile", "w") { |file| file.write("source 'https://rubygems.org'\n\nruby '#{version}'\n\ngem 'sinatra'") }
 
 items = ["a","b","c"]
 puts "Choose the items you want to add to your HTML site:\n
+ie: \"adf\" will give add Font Awesome, Materialize, and Material Design\n
 a. Font Awesome\n
 b. Burbon\n
+c. TwitterBootstrap\n
+d. Materialize\n
+e. Something\n
+f. Google's Material Design\n
 "
 choice = gets.chomp.downcase
 
 
 
-open("Open A") if choice.include?('a')
-open("Open B") if choice.include?('b')
+open("a") if choice.include?('a')
+open("b") if choice.include?('b')
+
+
 
 
 puts 'test'
@@ -46,24 +82,34 @@ Dir.chdir(pub)
 
 
 html = File.open("index.html", "w") { |file| file.write("
-  <link rel=\"stylesheet\" type=\"text/css\" href=\"reset.css\">
-  <link rel=\"stylesheet\" type=\"text/css\" href=\"stylesheet.css\">
+  <link rel=\"stylesheet\" type=\"text/css\" href=\"css/reset.css\">
+  <link rel=\"stylesheet\" type=\"text/css\" href=\"css/stylesheet.css\">
   <!DOCTYPE html>
 <html>
   <head>
     <meta charset=\"utf-8\">
-    <title></title>
+     <title></title>
+    ")}
+
+open("c") if choice.include?('c')
+open("d") if choice.include?('d')
+open("e") if choice.include?('e')
+open("f") if choice.include?('f')
+
+
+html = File.open("index.html", "a+"){|f| f << "
   </head>
   <body>
-
+  <h2>#{site.capitalize}</h2>
   </body>
 </html>
-")}
+"}
 
 Dir.mkdir("#{pub}/css")
 Dir.chdir("#{pub}/css")
 
 css = File.open("stylesheet.css", "w") { }
+
 reset = File.open("reset.css", "w") { |file| file.write("
   /* http://meyerweb.com/eric/tools/css/reset/
    v2.0 | 20110126
@@ -115,4 +161,10 @@ table {
 }
   ")}
 
-  puts "Dont Forget to bundle install your gems!!!!"
+  Dir.chdir(directory)
+
+  puts "Bundling"
+  %x(bundle install)
+  puts "Bundle Complete!"
+
+  %x(ruby server.rb)
